c69907a762677c77f555fa2eface52686f723000
poshangqiucao <poshangqiucao@gmail.com>
2020-04-30 18:36:38 +0800

    test Math.round() Math.floor() Math.ceil()

--- Math_test.js
+++ Math_test.js
@@ -1,4 +1,16 @@
 console.log(Math.E);
-console.log(Math.floor(2.1))
-console.log(Math.floor(2.9))
-console.log(Math.floor(2.5))
+
+// 向下取整
+console.log(Math.floor(2.1));
+console.log(Math.floor(2.9));
+console.log(Math.floor(2.5));
+
+// 向上取整
+console.log(Math.ceil(2.1));
+console.log(Math.ceil(2.9));
+console.log(Math.ceil(2.5));
+
+// 四舍五入
+console.log(Math.round(2.1));
+console.log(Math.round(2.9));
+console.log(Math.round(2.5));
